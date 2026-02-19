fa086e93c1ec47695c2bb868ce567374605838a2
Aleksandr Kasimov <sasha.asimo@ya.ru>
2026-02-19 10:24:22 +0300

    added fourth task solution

---
+++ Practice_2_data_types/Task4.java
@@ -0,0 +1,18 @@
+package Practice_2_data_types;
+
+public class Task4 {
+  public static void main(String[] args) {
+        int a = 1;
+        char c = 'a';
+        float f = 3.14f;
+        boolean b = false;
+        String Hi = "Hello ";
+        String name = "Echo";
+
+        System.out.println(Hi + a);
+        System.out.println(Hi + c);
+        System.out.println(Hi + f);
+        System.out.println(Hi + b);
+        System.out.println(Hi + name);
+    }  
+}
