7c07e5d95980ad2cc03bb1c85174da6d4781f9c2
Aleksandr Kasimov <sasha.asimo@ya.ru>
2026-03-05 12:52:37 +0300

    Операторы task 2

---
+++ Practice_4_Operators/task2.java
@@ -0,0 +1,21 @@
+package Practice_4_Operators;
+
+class Shape {}
+
+class Number {}
+
+class EvenNumber extends Number {}
+
+public class task2 {
+    public static void main(String[] args) {
+        EvenNumber even = new EvenNumber();
+        System.out.println("even instanceof EvenNumber = " + (even instanceof EvenNumber) ); 
+        System.out.println("even instanceof Number = " + (even instanceof Number) );
+        System.out.println("even instanceof Shape = " + (even instanceof Shape));
+        
+        //Проверка работы с null-объектом
+        EvenNumber nullObj = null;
+        System.out.println("nullPtr instanceof EvenNumber = " + (nullObj instanceof EvenNumber));
+        System.out.println("nullPtr instanceof EvenNumber = " + (nullObj instanceof Number));
+    }
+}
