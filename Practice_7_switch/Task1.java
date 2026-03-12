2e92d82852428f7c7bc1ef29d9008fd128d80e69
Aleksandr Kasimov <sasha.asimo@ya.ru>
2026-03-12 14:18:49 +0300

    made all expls

--- Practice_7_switch/Task1.java
+++ Practice_7_switch/Task1.java
@@ -2,6 +2,10 @@ package Practice_7_switch;
 
 import java.util.Scanner;
 
+enum Num {
+        FST, SEC
+    }
+
 public class Task1 {
     public static void main(String[] args) {
         //По аналогии работает с другими целочисленными типами и их классами-оболочками за исключением long, он не применим
@@ -23,14 +27,29 @@ public class Task1 {
 
         System.out.println(i);
 
-        int value = scan.nextInt();
-
+        //Так же применим char и его класс-оболочка
+        char someChar = 'a';
         i = 0;
-        switch(value) {
-            case 1:
+        switch (someChar) {
+            case 'a':
+                i = 1;
+                break;
+            case 'b':
+                i = 2;
+                break;
+            default:
+                i = 3;
+                break;
+        }
+
+        //Так же применим Sring
+        String str = scan.nextLine();
+        i = 0;
+        switch(str) {
+            case "FST":
                 i = 1; 
                 break;
-            case 2:
+            case "SEC":
                 i = 2; 
                 break;
             default:
@@ -40,7 +59,19 @@ public class Task1 {
 
         System.out.println(i);
 
+        //И enum
+        
+        Num num = Num.FST;
 
+        switch (num) {
+            case FST:
+                System.out.println("First day");
+                break;
+            case SEC:
+                System.out.println("Second day");
+                break;
+            
+        }
 
         scan.close();
     }
